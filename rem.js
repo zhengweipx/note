0a5346c8eaaf512eb07d4ac99ef4b52892c61e3e
zhengweipx <zhengweipx@163.com>
2018-05-30 11:55:38 +0800

    整数后加.00

---
+++ rem.js
@@ -0,0 +1,11 @@
+// 移动端设计尺寸
+var dh = 750;
+function resize() {
+    var cw = document.documentElement.clientWidth;
+    var bili = cw / dh * 100;
+    document.documentElement.style.fontSize = bili + "px";
+}
+window.addEventListener('resize', resize);
+resize();
+
+
